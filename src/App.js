74cc9d36064ac456e26609f282fe8d46b771b2cf
Edoardo Zampini <ezampini99@gmail.com>
2025-05-26 16:20:03 +0200

    Aggiunta struttura semantica

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
+import Header from './Header.js';
+import Nav from './Nav.js';
+import Main from './Main.js';
+import Footer from './Footer.js';
 
 function App() {
   return (
-    <div>
-      <h1>Homepage</h1>
-    </div>
+    <>
+      <Header />
+      <Nav />
+      <img src="https://www.littlelemon.com/logo.png" alt="Little Lemon Logo" />
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Menu</li>
+        <li>Reservations</li>
+        <li>Order Online</li>
+        <li>Login</li>
+      </ul>
+      <Main />
+      <Footer />
+      <img src="https://www.littlelemon.com/logo.png" alt="Little Lemon Logo" />
+      <ul>
+        <li>Contact</li>
+        <li>Doormat Navigation</li>
+        <li>Social Links</li>
+      </ul>
+    </>
+
   );
 }
 
